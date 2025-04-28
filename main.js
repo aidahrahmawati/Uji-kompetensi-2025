44f526bb2895e94816af1323551a0967ef2d481c
aidahrahmawati <aidahrahmawati04@gmail.com>
2025-04-28 09:47:33 +0700

    update main.js

--- main.js
+++ main.js
@@ -14,3 +14,19 @@ import {
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
+export async function ambildaftartugas() {
+  const refDokumen = collection(db, "senin");
+  const kueri = query(refDokumen, orderBy("tugas"));
+  const cuplikankueri = await getDocs(kueri);
+
+  let hasil = [];
+  cuplikankueri.forEach((dok) => {
+    hasil.push({
+      id: dok.id,
+      tugas: dok.data().tugas,
+      status: dok.data().status,
+      prioritas: dok.data().prioritas,
+      tanggal: dok.data().tanggal,
+    });
+  });
