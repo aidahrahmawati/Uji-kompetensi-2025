7cd9394e075f4b20abd88fd750d8c2321e3b78fb
aidahrahmawati <aidahrahmawati04@gmail.com>
2025-04-28 09:30:20 +0700

    update main.js

--- main.js
+++ main.js
@@ -11,3 +11,12 @@ import {
   orderBy,
   updateDoc
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNYqYJVYPTZRIK7pKZ4shbQ4igjseQpLM",
+  authDomain: "insan-cemerlang-d724d.firebaseapp.com",
+  projectId: "insan-cemerlang-d724d",
+  storageBucket: "insan-cemerlang-d724d.appspot.com",
+  messagingSenderId: "630693962922",
+  appId: "1:630693962922:web:a9447f760b858bcf781cd3"
+};
